c6601fce6b90b635006e696c7b957b354ca0b8a0
TamaraMayer <tamara-mayer@outlook.com>
2021-10-19 19:16:24 +0200

    Lab1 commit

---
+++ Lab1/lab1ApplicationTM/routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Lab 1 Application Tamara Mayer' });
+});
+
+module.exports = router;
